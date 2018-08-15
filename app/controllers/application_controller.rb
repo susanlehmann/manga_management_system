4201f2495201d6aa93dc21b7558b00b265543398
ZzkilluazZ <killua19931993@gmail.com>
2018-08-15 13:24:56 +0700

    change role and delete user by admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
+
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+
   def set_locale
     I18n.locale = params[:locale] if params[:locale].present?
   end
