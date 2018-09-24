f51751c86bd95485b1c288853a4af00c08822b35
NHDang95 <2d2l95@gmail.com>
2018-09-24 11:35:05 +0700

    minor_fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,9 +32,10 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do | user |
       user.email = auth.info.email
+      user.name = auth.info.name
       user.provider = auth.provider
       user.uid = auth.uid
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0,10]
     end
   end
 
